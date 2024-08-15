c5db5c0e371f1709f90732067e36fbb865c4b8d7
Touseef <mtouseef897@gmail.com>
2024-08-15 12:53:29 +0500

    responsiveness adjusted

--- components/Button.jsx
+++ components/Button.jsx
@@ -10,7 +10,7 @@ const Button = ({ children, icon = false, next = true,className="" }) => {
   return (
     <div className={`p-[10px] ${next?"bg-[#DA7B22]":"bg-[#222323]"}  rounded-2xl  ${className} z-50 active:scale-95 duration-200 ease-in-out mt-16 md:mt-auto`}>
       <button
-        className={`${font_Anton.className} text-[22px] uppercase leading-[33.12px] tracking-wide py-[15px] px-[32px] ${next?"bg-[#E69244]":"bg-[#343434]"} rounded-2xl -mt-4 flex items-center gap-4 `}
+        className={`${font_Anton.className} text-[22px] uppercase leading-[33.12px] tracking-wide py-[8px] px-[12px]  md:py-[15px] md:px-[32px] ${next?"bg-[#E69244]":"bg-[#343434]"} rounded-2xl -mt-4 flex items-center gap-4 `}
         onClick={() => updatePageNumber(next)}
       >{icon && (!next) && <FaArrowLeft />}
         {children} {icon && next &&<FaArrowRight />}
